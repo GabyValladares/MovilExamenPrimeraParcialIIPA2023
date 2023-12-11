c90e99774ef781bebc585f96c251d23906fbf9fa
Henry Paul Diaz Pinchao <henry.diaz054@ist17dejulio.edu.ec>
2023-12-11 12:24:21 -0500

    Comunicacion Backend y Frontend en segunda actividad

--- app/src/main/java/com/ggvc/examenprimeraparcialmovil/Matricula.java
+++ app/src/main/java/com/ggvc/examenprimeraparcialmovil/Matricula.java
@@ -3,12 +3,21 @@ package com.ggvc.examenprimeraparcialmovil;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Matricula extends AppCompatActivity {
 
+    TextView lblRenovacion, lblContaminacion, lblMulta, lblValor, lblTotal;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_matricula);
+        lblRenovacion = findViewById(R.id.lblRenovacion);
+        lblContaminacion = findViewById(R.id.lblContaminacion);
+        lblMulta = findViewById(R.id.lblMultasRe);
+        lblValor = findViewById(R.id.lblValorRe);
+        lblTotal = findViewById(R.id.lblTotal);
+
+        Bundle datos = getIntent().getExtras();
     }
 }
