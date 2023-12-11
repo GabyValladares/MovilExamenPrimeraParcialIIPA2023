6cf269d351976b2d0b77b1118ddef6a699b12808
MiltonRamos2002 <milton.ramos954@ist17dejulio.edu.ec>
2023-12-11 13:04:53 -0500

    Ingreso de parametros

---
+++ app/src/main/java/com/ggvc/examenprimeraparcialmovil/VistaActivity.java
@@ -0,0 +1,22 @@
+package com.ggvc.examenprimeraparcialmovil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class VistaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vista);
+        TextView tvIn;
+        TextView tvEgre;
+        TextView tvTot;
+        Button btnRegre;
+        Button btnSalir;
+
+    }
+}
