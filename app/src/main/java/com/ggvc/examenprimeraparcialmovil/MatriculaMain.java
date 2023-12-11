c714c998002c7546699d7d6c5171de7a36a6a727
Henry Paul Diaz Pinchao <henry.diaz054@ist17dejulio.edu.ec>
2023-12-11 11:44:20 -0500

    Creacion Actividad main con los todos los parametros necesarios

---
+++ app/src/main/java/com/ggvc/examenprimeraparcialmovil/MatriculaMain.java
@@ -0,0 +1,14 @@
+package com.ggvc.examenprimeraparcialmovil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MatriculaMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_matricula_main);
+    }
+}
