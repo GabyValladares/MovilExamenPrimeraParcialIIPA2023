f6bae4f5b6e93d71e60e555c6a15312574f0b362
RyCry7 <richely.almeida825@ist17dejulio.edu.ec>
2023-12-11 11:55:52 -0500

    frontend activity Main

--- app/src/main/java/com/rycry/movilexamenprimeraparcial/MainActivity.java
+++ app/src/main/java/com/rycry/movilexamenprimeraparcial/MainActivity.java
@@ -1,8 +1,9 @@
 package com.rycry.movilexamenprimeraparcial;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Obtén referencia al Spinner desde el archivo de diseño (layout)
+        Spinner spnMarcaModelo = findViewById(R.id.spnMarcaModelo);
+
+        // Define las opciones en un array de cadenas
+        String[] opciones = {"Toyota-Jeep", "Toyota-Camioneta", "Suzuki-Vitara", "Suzuki-Automóvil"};
+
+        // Crea un adaptador para el Spinner
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, opciones);
+
+        // Especifica el diseño para el menú desplegable
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Aplica el adaptador al Spinner
+        spnMarcaModelo.setAdapter(adapter);
     }
 }
