5f0f4f8dfbcdaddfe6b72fdd1cb2b5c9040252ea
JefersonYandun <jeferson.yandun237@ist17dejulio.edu.ec>
2023-12-11 13:29:42 -0500

    IMPMENETACION BOTON ATRAS Y CALCULOS DE FUNCIONES MULTAS

--- app/src/main/java/com/ggvc/examenprimeraparcialmovil/activity_formulario_vehiculo.java
+++ app/src/main/java/com/ggvc/examenprimeraparcialmovil/activity_formulario_vehiculo.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+import java.util.Calendar;
 
 public class activity_formulario_vehiculo extends AppCompatActivity {
 
@@ -41,7 +43,7 @@ public class activity_formulario_vehiculo extends AppCompatActivity {
     }
 
     private void procesarCalculos() {
-        // Obtener los valores ingresados por el usuario
+
         String cedula = txtCedula.getText().toString();
         String placa = txtPlaca.getText().toString();
         String anio = txtAnio.getText().toString();
@@ -49,27 +51,125 @@ public class activity_formulario_vehiculo extends AppCompatActivity {
         String tipoVehiculo = txtTipoVehiculo.getText().toString();
         String multas = txtMultas.getText().toString();
 
-        // Realizar la lógica de cálculos aquí (usar Double.parseDouble para convertir a números)
-        double importeRenovacion = 50.0; // Reemplazar con la lógica real
-        double valorMatricula = 100.0;   // Reemplazar con la lógica real
-        double multaContaminacion = 20.0; // Reemplazar con la lógica real
-        double multaPorMultas = 30.0;    // Reemplazar con la lógica real
 
-        // Calcular el total a pagar
+        if (cedula.isEmpty() || placa.isEmpty() || anio.isEmpty() || marca.isEmpty() || tipoVehiculo.isEmpty() || multas.isEmpty()) {
+            Toast.makeText(this, "Por favor, complete todos los campos", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        double importeRenovacion = calcularImporteRenovacion(cedula, placa);
+        double multaContaminacion = calcularMultaContaminacion(anio);
+
+        // Puedes asignar un valor de ejemplo para valorVehiculo o cambiar el método según tus necesidades
+        double valorVehiculo = 10000.0;
+        double valorMatricula = calcularValorMatricula(marca, tipoVehiculo, valorVehiculo);
+
+        double multaPorMultas = multas.equalsIgnoreCase("SI") ? calcularMultaPorMultas("si") : 0.0;
+
         double totalPagar = importeRenovacion + valorMatricula + multaContaminacion + multaPorMultas;
 
-        // Crear un Intent para pasar a la actividad de resultados
+
         Intent intent = new Intent(activity_formulario_vehiculo.this, activity_resultados_vehiculo.class);
 
-        // Puedes agregar datos adicionales al Intent si es necesario
         intent.putExtra("IMPORTE_RENOVACION", importeRenovacion);
         intent.putExtra("VALOR_MATRICULA", valorMatricula);
         intent.putExtra("MULTA_CONTAMINACION", multaContaminacion);
         intent.putExtra("MULTA_POR_MULTAS", multaPorMultas);
         intent.putExtra("TOTAL_PAGAR", totalPagar);
 
-        // Iniciar la actividad de resultados
+
         startActivity(intent);
     }
-}
 
+
+
+    private double calcularImporteRenovacion(String cedula, String placa) {
+        // Supongamos que el 5% del sueldo básico es $435
+        double sueldoBasico = 435.0;
+        double porcentajeRenovacion = 0.05;
+
+        // Verificar si el número de cédula comienza con "1" y la placa contiene la letra "I"
+        if (cedula.startsWith("1") && placa.contains("I")) {
+            // Calcular el importe de renovación
+            double importeRenovacion = sueldoBasico * porcentajeRenovacion;
+            return importeRenovacion;
+        } else {
+            // Si no cumple con las condiciones, el importe es cero
+            return 0.0;
+        }
+    }
+
+
+
+    private double calcularMultaContaminacion(String anio) {
+        // Obtener el año actual
+        int anioActual = Calendar.getInstance().get(Calendar.YEAR);
+
+        // Convertir el año de fabricación a entero
+        int anioFabricacion = Integer.parseInt(anio);
+
+        // Calcular la cantidad de años de contaminación
+        int aniosContaminacion = anioActual - anioFabricacion;
+
+        // Verificar si la multa es aplicable (solo para vehículos con más de 10 años)
+        if (aniosContaminacion > 10) {
+            // Supongamos que la multa es del 2% por cada año de contaminación
+            double porcentajeMulta = 0.02;
+
+            // Calcular la multa por contaminación
+            double multaContaminacion = porcentajeMulta * aniosContaminacion;
+
+            return multaContaminacion;
+        } else {
+            // Si el vehículo tiene 10 años o menos, la multa es cero
+            return 0.0;
+        }
+    }
+
+
+    private double calcularValorMatricula(String marca, String tipoVehiculo, double valorVehiculo) {
+        // Definir porcentajes de matriculación para diferentes combinaciones de marca y tipo de vehículo
+        double porcentajeToyotaJeep = 0.08;
+        double porcentajeToyotaCamioneta = 0.12;
+        double porcentajeSuzukiVitara = 0.10;
+        double porcentajeSuzukiAutomovil = 0.09;
+
+        // Calcular el valor de matriculación según la combinación de marca y tipo de vehículo
+        if (marca.equalsIgnoreCase("Toyota")) {
+            if (tipoVehiculo.equalsIgnoreCase("Jeep")) {
+                return valorVehiculo * porcentajeToyotaJeep;
+            } else if (tipoVehiculo.equalsIgnoreCase("Camioneta")) {
+                return valorVehiculo * porcentajeToyotaCamioneta;
+            }
+        } else if (marca.equalsIgnoreCase("Suzuki")) {
+            if (tipoVehiculo.equalsIgnoreCase("Vitara")) {
+                return valorVehiculo * porcentajeSuzukiVitara;
+            } else if (tipoVehiculo.equalsIgnoreCase("Automóvil")) {
+                return valorVehiculo * porcentajeSuzukiAutomovil;
+            }
+        }
+
+        // Valor de matriculación por defecto si no se encuentra una combinación válida
+        return 0.0;
+    }
+
+
+    private double calcularMultaPorMultas(String tieneMultas) {
+        // Supongamos que el sueldo básico es $435 y la multa es del 25%
+        double sueldoBasico = 435.0;
+        double porcentajeMulta = 0.25;
+
+        // Lógica para calcular la multa por multas
+        // Puedes utilizar el parámetro tieneMultas o agregar lógica adicional según tus necesidades
+
+        // Calcular la multa por multas si el vehículo tiene multas
+        if ("si".equalsIgnoreCase(tieneMultas)) {
+            return sueldoBasico * porcentajeMulta;
+        }
+
+        // Si no tiene multas, la multa es cero
+        return 0.0;
+    }
+
+
+}
