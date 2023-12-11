d3178b2b13dde1b64693dfdf395e64773a3794c5
MiguelBrucil <132540041+MiguelBrucil@users.noreply.github.com>
2023-12-11 13:25:01 -0500

    fINALIZACION DE DATOS

--- app/src/main/java/com/ggvc/examenprimeraparcialmovil/DatosEnviados.java
+++ app/src/main/java/com/ggvc/examenprimeraparcialmovil/DatosEnviados.java
@@ -1,8 +1,8 @@
 package com.ggvc.examenprimeraparcialmovil;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class DatosEnviados extends AppCompatActivity {
 
@@ -10,5 +10,64 @@ public class DatosEnviados extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_datos_enviados);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String cedula = extras.getString("cedula");
+            String nombreCompleto = extras.getString("nombreCompleto");
+            String placa = extras.getString("placa");
+            int anioFabricacion = extras.getInt("anioFabricacion");
+            String color = extras.getString("color");
+            String tipoVehiculo = extras.getString("tipoVehiculo");
+            String multa = extras.getString("multa");
+
+            double sueldoBasico = 435.0;
+            double impuestoRenovacionPlacas = 0.0;
+            double multaContaminacion = 0.0;
+            double valorMatriculacion = 0.0;
+            double multaPorcentajeSueldo = 0.0;
+
+            if (cedula.startsWith("1") && placa.startsWith("I")) {
+                impuestoRenovacionPlacas = 0.05 * sueldoBasico;
+            }
+
+            if (anioFabricacion < 2010) {
+                multaContaminacion = 0.02 * sueldoBasico * (2010 - anioFabricacion);
+            }
+
+            if (tipoVehiculo != null) {
+                if (tipoVehiculo.equals("Jeep")) {
+                    valorMatriculacion = 0.08 * sueldoBasico;
+                } else if (tipoVehiculo.equals("Camioneta")) {
+                    valorMatriculacion = 0.12 * sueldoBasico;
+                } else if (tipoVehiculo.equals("Vitara")) {
+                    valorMatriculacion = 0.10 * sueldoBasico;
+                } else if (tipoVehiculo.equals("Automóvil")) {
+                    valorMatriculacion = 0.09 * sueldoBasico;
+                }
+            }
+
+            if (multa != null && multa.equals("true")) {
+                multaPorcentajeSueldo = 0.25 * sueldoBasico;
+            }
+
+            double totalPagar = impuestoRenovacionPlacas + multaContaminacion + valorMatriculacion + multaPorcentajeSueldo;
+
+            String mensaje = "Cédula: " + cedula + "\n"
+                    + "Nombre: " + nombreCompleto + "\n"
+                    + "Placa: " + placa + "\n"
+                    + "Año de Fabricación: " + anioFabricacion + "\n"
+                    + "Color: " + color + "\n"
+                    + "Tipo de Vehículo: " + tipoVehiculo + "\n"
+                    + "Impuesto Renovación Placas: $" + impuestoRenovacionPlacas + "\n"
+                    + "Multa Contaminación: $" + multaContaminacion + "\n"
+                    + "Valor Matriculación: $" + valorMatriculacion + "\n"
+                    + "Multa Porcentaje Sueldo: $" + multaPorcentajeSueldo + "\n"
+                    + "Total a Pagar: $" + totalPagar;
+
+            Toast.makeText(this, mensaje, Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "No se recibieron datos", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/ggvc/examenprimeraparcialmovil/Datos_Vehiculos.java
+++ app/src/main/java/com/ggvc/examenprimeraparcialmovil/Datos_Vehiculos.java
@@ -1,8 +1,16 @@
 package com.ggvc.examenprimeraparcialmovil;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Objects;
 
 public class Datos_Vehiculos extends AppCompatActivity {
 
@@ -10,5 +18,57 @@ public class Datos_Vehiculos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_datos_vehiculos);
+
+        @SuppressLint("WrongViewCast") EditText edCedula = findViewById(R.id.txtCedulaP);
+        @SuppressLint("WrongViewCast") EditText edNombre = findViewById(R.id.txtNombreP);
+        @SuppressLint("WrongViewCast") EditText edPlaca = findViewById(R.id.txtPlaca);
+        @SuppressLint("WrongViewCast") EditText edAnio = findViewById(R.id.txtAnio);
+        @SuppressLint("WrongViewCast") EditText edColor = findViewById(R.id.txtColor);
+        @SuppressLint("WrongViewCast") EditText edTvVehiculo = findViewById(R.id.txtTvehiculo);
+        @SuppressLint("WrongViewCast") EditText edMulta = findViewById(R.id.txtMulta);
+
+        Button btnCalcular = findViewById(R.id.btnSigiente);
+
+        btnCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Datos_Vehiculos.this, DatosEnviados.class);
+                Bundle datos = new Bundle();
+
+                String cedula = Objects.requireNonNull(edCedula.getText()).toString();
+                String nombreCompleto = Objects.requireNonNull(edNombre.getText()).toString();
+                String placa = Objects.requireNonNull(edPlaca.getText()).toString();
+                String anioStr = Objects.requireNonNull(edAnio.getText()).toString();
+                String color = Objects.requireNonNull(edColor.getText()).toString();
+                String tipoVehiculo = Objects.requireNonNull(edTvVehiculo.getText()).toString();
+                String multa = Objects.requireNonNull(edMulta.getText()).toString();
+
+                try {
+                    Integer anio = Integer.parseInt(anioStr);
+
+                    if (!TextUtils.isEmpty(cedula) && !TextUtils.isEmpty(nombreCompleto)
+                            && !TextUtils.isEmpty(placa) && anio != null
+                            && !TextUtils.isEmpty(color) && !TextUtils.isEmpty(tipoVehiculo)
+                            && !TextUtils.isEmpty(multa)) {
+                        Toast.makeText(Datos_Vehiculos.this, "CALCULANDO DATOS DEL VEHÍCULO", Toast.LENGTH_SHORT).show();
+
+                        datos.putString("cedula", cedula);
+                        datos.putString("nombreCompleto", nombreCompleto);
+                        datos.putString("placa", placa);
+                        datos.putInt("anioFabricacion", anio);
+                        datos.putString("color", color);
+                        datos.putString("tipoVehiculo", tipoVehiculo);
+                        datos.putString("multa", multa);
+
+                        intent.putExtras(datos);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(Datos_Vehiculos.this, "INGRESE DATOS VÁLIDOS", Toast.LENGTH_SHORT).show();
+                    }
+                } catch (NumberFormatException e) {
+                    Toast.makeText(Datos_Vehiculos.this, "ASEGÚRESE DE LLENAR BIEN LOS CAMPOS", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
